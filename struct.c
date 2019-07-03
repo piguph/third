30f6e1d1875665a332a806796229eae4a760b45b
handsome boy <13612264208@163.com>
2019-07-03 22:49:53 +0800

    struct

---
+++ struct.c
@@ -0,0 +1,21 @@
+//#include<stdio.h>
+//#include<stdlib.h>
+//#include<string.h>
+//
+//#pragma pack(4)		//手动定义以多少个字节对齐	
+//
+//struct Stu
+//{
+//	char c;
+//	double d;		
+//	int a;
+//	short b;
+//};
+//
+//int main()
+//{
+//	struct Stu stu;
+//	printf("%u,%u\n", sizeof(struct Stu), sizeof(stu));		//结构体大小跟声明变量的顺序有关，不是固定的值，根据内存对齐来计算
+//	system("pause");
+//	return 0;
+//}
